82761de39704c1c7fe157057bc13ec46ec6b881a
esquinasbarstm <esquinasbarstm@gmail.com>
2025-06-12 19:13:58 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,9 @@
-
+// Importações do Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
+// 🔧 Configuração do seu projeto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDqyuCh4pOphH_Izal6hoytjy3CZG5XkT8",
   authDomain: "esquinas-match.firebaseapp.com",
@@ -12,8 +13,9 @@ const firebaseConfig = {
   appId: "1:1047206691859:web:b3f4b94c00098843762be6"
 };
 
+// 🚀 Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { db, storage };
+// 🔗 Exporta Firestore e Storage para uso nos outros scripts
+export const db = getFirestore(app);
+export const storage = getStorage(app);
