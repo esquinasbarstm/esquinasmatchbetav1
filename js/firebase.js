7d5656095e217eec67cb3e78bb237fa353a59149
esquinasbarstm <esquinasbarstm@gmail.com>
2025-06-12 21:02:48 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,18 @@
-// Importações do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-storage.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-// Configuração do seu projeto Firebase (com bucket correto)
 const firebaseConfig = {
   apiKey: "AIzaSyDqyuCh4pOphH_Izal6hoytjy3CZG5XkT8",
   authDomain: "esquinas-match.firebaseapp.com",
   projectId: "esquinas-match",
-  storageBucket: "esquinas-match.firebasestorage.app", // ✅ bucket certo
+  storageBucket: "esquinas-match.appspot.com",
   messagingSenderId: "1047206691859",
   appId: "1:1047206691859:web:b3f4b94c00098843762be6"
 };
 
-// Inicialização
 const app = initializeApp(firebaseConfig);
 
-// Exportando Firestore e Storage
 export const db = getFirestore(app);
 export const storage = getStorage(app);
