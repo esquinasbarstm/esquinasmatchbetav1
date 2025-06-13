2840793432cc85556e5a25b7d1ec4adfe0cdfea5
esquinasbarstm <esquinasbarstm@gmail.com>
2025-06-12 21:13:57 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,4 @@
+
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
@@ -13,6 +14,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
 export const storage = getStorage(app);
